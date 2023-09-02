90a44d50cdc985110f2ae8b452b9466d216f5f87
Agustin Bustos Piasentini bustos-agustin@hotmail.com <63634574+abustosp@users.noreply.github.com>
2023-09-02 01:58:21 -0300

    Add GUI

---
+++ BIN/ProcesarCSVHolistorCompras.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import numpy as np
+from tkinter.messagebox import showinfo
+
+def Procesar_CSV(Archivos : list):
+    '''
+    Procesar archivos de CSV en base a una lista de ubicaciones
+
+
+    '''
+    # Reemplazar de la lista todos los '\\' por '/'
+    Archivos = [i.replace('\\' , '/') for i in Archivos]
+
+    # Eliminar los archivos que no sean CSV
+    Archivos = [i for i in Archivos if i.split(".")[-1] == "csv"]
+
+    for i in Archivos:
+
+        # Leer tipos de comprobantes
+        Tipos_Comprobantes = pd.read_excel("TABLACOMPROBANTES.xls")
+
+        # Leer Proveedores
+        Proveedores = pd.read_csv("Proveedores.csv" , sep=";")
+
+        # Leer hojas de Excel para agregarlas al archivo final
+        Provincias = pd.read_excel("Modelo-Holistor-Compras.xls" , sheet_name="Provincias")
+        Tipo_DOC = pd.read_excel("Modelo-Holistor-Compras.xls" , sheet_name="Tipo Doc.")
+
+        # Renombrar 'Código' por 'Código Provincia' en 'Provincias'
+        Provincias.rename(columns={'Código ': 'Código Provincia'} , inplace=True)
+
+        # Hacer un trim en la columna de 'Provincias'
+        Provincias['Provincias'] = Provincias['Provincias'].str.strip()
+
+        # hacer un marge de Proveedores y Provincias
+        Proveedores = pd.merge(Proveedores , 
+                            Provincias , 
+                            how='left' , 
+                            left_on='Provincia' , 
+                            right_on='Provincias')
+        Proveedores.drop('Provincias' , axis=1 , inplace=True)
+
+        Proveedores.rename(columns={'Tipo': 'Tipo Responsable'} , inplace=True)
+
+
+        # Leer el archivo CSV
+        Compras = pd.read_csv(i , sep=";" , decimal=",")
+
+
+        # Converir la columna 'Fecha de Emisión' a datetime de formato %Y-%m-%d
+        Compras['Fecha de Emisión'] = pd.to_datetime(Compras['Fecha de Emisión'] , format='%Y-%m-%d')
+
+        # Mostrar como %d/%m/%Y la columna 'Fecha de Emisión'
+        Compras['Fecha de Emisión'] = Compras['Fecha de Emisión'].dt.strftime('%d/%m/%Y')
+
+        # Columnas a sumar a 'Otros Tributos'
+        Otros = ['Importe de Per. o Pagos a Cta. de Otros Imp. Nac.' , 'Importe de Impuestos Municipales' , 'Importe de Impuestos Internos' , 'Importe Otros Tributos']
+        Compras['Otros Tributos'] = Compras[Otros].sum(axis=1)
+        Compras.drop(Otros , axis=1 , inplace=True)
+        del Otros
+
+        # Eliminar columnas
+        Columnas_Eliminar = ['Total Neto Gravado' , 'Total IVA' , 'Crédito Fiscal Computable', ]
+        Compras.drop(Columnas_Eliminar , axis=1 , inplace=True)
+        del Columnas_Eliminar
+
+        # Hacer un melt de las columnas de 'Neto Gravado IVA 0%' , 'Neto Gravado IVA 2,5%' , 'Importe IVA 2,5%' , 'Neto Gravado IVA 5%' , 'Importe IVA 5%' , 'Neto Gravado IVA 10,5%' , 'Importe IVA 10,5%' , 'Neto Gravado IVA 21%' , 'Importe IVA 21%' , 'Neto Gravado IVA 27%' , 'Importe IVA 27%' en 3 columnas: 'Neto Gravado' , 'Importe IVA' , 'Alicuota IVA'
+        Compras = pd.melt(Compras, id_vars=['Fecha de Emisión' , 'Tipo de Comprobante' , 'Punto de Venta' , 'Número de Comprobante' , 'Tipo Doc. Vendedor' , 'Nro. Doc. Vendedor' , 'Denominación Vendedor' , 'Importe Total' , 'Moneda Original' , 'Tipo de Cambio' , 'Importe No Gravado' , 'Importe Exento' , 'Importe de Percepciones de Ingresos Brutos' , 'Importe de Percepciones o Pagos a Cuenta de IVA' , 'Otros Tributos' ] ,  value_vars=['Neto Gravado IVA 0%' , 'Neto Gravado IVA 2,5%' , 'Importe IVA 2,5%' , 'Neto Gravado IVA 5%' , 'Importe IVA 5%' , 'Neto Gravado IVA 10,5%' , 'Importe IVA 10,5%' , 'Neto Gravado IVA 21%' , 'Importe IVA 21%' , 'Neto Gravado IVA 27%' , 'Importe IVA 27%'] , var_name='Tipo IVA' , value_name='Monto IVA')
+
+        # Filtrar los que 'Monto IVA' sea distinto de np.nan
+        Compras = Compras[Compras['Monto IVA'].notna()]
+
+        # Separar la columna 'Tipo IVA' en 2 columnas: 'Alicuota IVA' y 'Tipo IVA' donde 'Alicuota IVA' es el ultimo caracter de 'Tipo IVA'
+        Compras['Alicuota IVA'] = Compras['Tipo IVA'].str.split(' ').str[-1]
+        Compras['Tipo IVA'] = Compras['Tipo IVA'].str.split(' ').str[0:-2].str.join(' ')
+
+        # Filtar los que 'Tipo IVA' sea 'Importe'
+        Compras = Compras[Compras['Tipo IVA'] != 'Importe']
+        Compras.drop('Tipo IVA' , axis=1 , inplace=True)
+
+        Compras['Alicuota IVA'] = Compras['Alicuota IVA'].str.replace('%' , '').str.replace("," , ".").astype(float) /100
+        Compras['Importe IVA'] = round( (Compras['Monto IVA'] * Compras['Alicuota IVA']) , 2)
+
+        # Renombrar columna 'Monto IVA' a 'Neto Gravado'
+        Compras.rename(columns={'Monto IVA': 'Neto Gravado'} , inplace=True)
+
+        # Merge de 'Compras' y 'Tipos_Comprobantes'
+        Compras = pd.merge(Compras ,
+                            Tipos_Comprobantes[['Código CBTE' ,'Letra CBTE' , 'Tipo CBTE' ]] ,
+                            how='left' ,
+                            left_on='Tipo de Comprobante' ,
+                            right_on='Código CBTE')
+        Compras.drop('Código CBTE' , axis=1 , inplace=True)
+        del Tipos_Comprobantes
+
+        # Merge de 'Compras' y 'Proveedores'
+        Compras = pd.merge(Compras ,
+                            Proveedores[['CUIT' , 'Tipo Responsable' , 'Domicilio' , 'Código Provincia']] ,
+                            how='left' ,
+                            left_on=['Nro. Doc. Vendedor'] ,
+                            right_on=['CUIT'])
+        Compras.drop('CUIT' , axis=1 , inplace=True)
+        del Proveedores
+
+        Compras['Fecha de Recepción'] = Compras['Fecha de Emisión']
+        Compras['C.P'] = np.nan
+        Compras['Cód. Neto'] = "CMG"
+        Compras ['CF Computable'] = Compras['Importe IVA']
+        Compras['Cód. NG/EX'] = "NGC"
+        Compras['NG + E'] = Compras['Importe No Gravado'] + Compras['Importe Exento'] + Compras['Otros Tributos']
+        Compras['Cód. P/R'] = np.nan
+        Compras['RETPER'] = Compras['Importe de Percepciones de Ingresos Brutos'] + Compras['Importe de Percepciones o Pagos a Cuenta de IVA']
+        Compras['Pcia P/R'] = Compras['Código Provincia']
+
+        # Reordenar columnas en otro DataFrame
+        Columnas_Exportar = ['Fecha de Emisión' , 'Fecha de Recepción' , 'Tipo CBTE' , 'Letra CBTE' , 'Punto de Venta' , 'Número de Comprobante' , 'Denominación Vendedor' , 'Tipo Doc. Vendedor' , 'Nro. Doc. Vendedor' , 'Domicilio' , 'C.P' , 'Código Provincia' , 'Tipo Responsable' , 'Cód. Neto' , 'Neto Gravado' , 'Alicuota IVA' , 'Importe IVA' , 'CF Computable' , 'Cód. NG/EX' , 'NG + E' , 'Cód. P/R' , 'RETPER' , 'Pcia P/R' , 'Importe Total'  ]
+        Compras_Ordenado = Compras[Columnas_Exportar]
+
+        # Encuentra las filas duplicadas basadas en las columnas especificadas
+        duplicates = Compras_Ordenado.duplicated(subset=['Tipo CBTE', 'Punto de Venta', 'Número de Comprobante', 'Nro. Doc. Vendedor'])
+
+        # Utiliza loc para asignar 0 a las filas duplicadas en 'NG + E' y 'RETPER'
+        Compras_Ordenado.loc[duplicates, ['NG + E', 'RETPER']] = 0
+
+        # Ordenar por 'Nro. Doc. Vendedor', 'Punto de Venta' y 'Número de Comprobante'
+        Compras_Ordenado.sort_values(by=['Nro. Doc. Vendedor' , 'Punto de Venta' , 'Número de Comprobante'] , inplace=True)
+
+        # Renomrar las columnas de Provincias y Tipo_DOC a sus nombres originales
+        Provincias.rename(columns={'Código Provincia': 'Código '} , inplace=True)
+
+        # Obtener la ubicación de i
+        Nombre_Archivo = str(i).split("/")[-1]
+        Path = str(i).replace(Nombre_Archivo , "")
+
+        # Exportar Compras_Ordenado, Provincias y Tipo_Doc a Excel XLS
+        with pd.ExcelWriter(f'{Path}{Nombre_Archivo.replace(".CSV" , "")} - Procesado para Holisor.xls') as writer:
+            Compras_Ordenado.to_excel(writer, sheet_name='HWCompra-modelo', index=False)
+            Provincias.to_excel(writer, sheet_name='Provincias', index=False)
+            Tipo_DOC.to_excel(writer, sheet_name='Tipo Doc.', index=False)
+
+    # Mostrar mensaje de finalización
+    showinfo("Finalizado" , "Se han procesado todos los archivos y se han guardado en la misma ubicación que los archivos originales con el nombre de 'NombreArchivo - Procesado para Holistor.xls'")
---
+++ GUI-CSV-Holistor.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+import tkinter as tk
+import tkinter.ttk as ttk
+from BIN.ProcesarCSVHolistorCompras import Procesar_CSV
+from tkinter.filedialog import askdirectory
+
+def Cafecito():
+    import webbrowser
+    webbrowser.open_new("https://cafecito.app/abustos")
+
+def Carpeta():
+    Carpeta = askdirectory()
+    import os
+    Archivos = os.listdir(Carpeta)
+    Archivos = [Carpeta + "/" + i for i in Archivos]
+    Procesar_CSV(Archivos)
+
+def Abrir_Excel():
+    import os
+    os.system("start EXCEL.EXE Lista-Archivos.xlsx")
+    
+def Procesar_Excel():
+    import pandas as pd
+    df = pd.read_excel("Lista-Archivos.xlsx")
+    df = df[df['Raiz'].notna()]
+    Archivos = df['Lista'].tolist()
+    Procesar_CSV(Archivos)
+
+
+class ModeloPygubuApp:
+    def __init__(self, master=None):
+        # build ui
+        Toplevel_1 = tk.Tk() if master is None else tk.Toplevel(master)
+        Toplevel_1.configure(
+            background="#2e2e2e",
+            cursor="arrow",
+            height=250,
+            width=335)
+        Toplevel_1.iconbitmap("BIN/ABP-blanco-en-fondo-negro.ico")
+        Toplevel_1.overrideredirect("False")
+        Toplevel_1.resizable(False, False)
+        Toplevel_1.title("CSV a Holistor")
+        # First object created
+        #self.setup_ttk_styles(Toplevel_1)
+
+        Label_3 = ttk.Label(Toplevel_1)
+        self.img_ABPblancoenfondonegro111 = tk.PhotoImage(
+            file="BIN/ABP-blanco-sin-fondo .png")
+        Label_3.configure(
+            background="#2e2e2e",
+            image=self.img_ABPblancoenfondonegro111)
+        Label_3.pack(side="top")
+        Label_1 = ttk.Label(Toplevel_1)
+        Label_1.configure(
+            background="#2e2e2e",
+            foreground="#ffffff",
+            justify="center",
+            takefocus=False,
+            text='Procesar CSV del Portal IVA al papel de trabajo de importación de Holistor\n',
+            wraplength=320)
+        Label_1.pack(expand=True, side="top")
+        Label_2 = ttk.Label(Toplevel_1)
+        Label_2.configure(
+            background="#2e2e2e",
+            foreground="#ffffff",
+            justify="center",
+            text='por Agustín Bustos Piasentini\nhttps://www.Agustin-Bustos-Piasentini.com.ar/')
+        Label_2.pack(expand=True, side="top")
+        self.Carpeta = ttk.Button(Toplevel_1)
+        self.Carpeta.configure(text='Seleccionar Carpeta con CSV', command=Carpeta)
+        self.Carpeta.pack(expand=True, pady=4, side="top")
+        self.Abrir_Excel = ttk.Button(Toplevel_1)
+        self.Abrir_Excel.configure(text='Abrir Excel con Ubicaciones', command=Abrir_Excel)
+        self.Abrir_Excel.pack(expand=True, pady=4, side="top")
+        self.Procesar_Excel = ttk.Button(Toplevel_1)
+        self.Procesar_Excel.configure(text='Procesar Lista del Excel' , command=Procesar_Excel)
+        self.Procesar_Excel.pack(expand=True, pady=4, side="top")
+        self.Donación = ttk.Button(Toplevel_1)
+        self.Donación.configure(text='Colaboraciones' , command=Cafecito)
+        self.Donación.pack(pady=4, side="top")
+
+        # Main widget
+        self.mainwindow = Toplevel_1
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+
+if __name__ == "__main__":
+    app = ModeloPygubuApp()
+    app.run()
